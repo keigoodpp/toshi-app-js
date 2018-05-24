bd793d44a854a95bac9b147302f98fc5cc2f5f89
keigoodpp <39551086+keigoodpp@users.noreply.github.com>
2018-05-24 12:44:44 +0900

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -95,6 +95,7 @@ function donate(session) {
 function sendMessage(session, message) {
   let controls = [
     {type: 'button', label: 'Ping', value: 'ping'},
+    {type: 'button', label: 'Ping', value: 'ping'},
     {type: 'button', label: 'Count', value: 'count'},
     {type: 'button', label: 'Donate', value: 'donate'}
   ]
